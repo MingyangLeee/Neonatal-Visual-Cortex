6d04c5ab3966189ee6fe0275d8880fb4dc4c61c8
MingyangLeee <103493421+MingyangLeee@users.noreply.github.com>
2022-04-13 10:04:41 +0800

    Update NVCP_wb_vol2surf_tri.m

--- CodesFlow/Functions/Surf/NVCP_wb_vol2surf_tri.m
+++ CodesFlow/Functions/Surf/NVCP_wb_vol2surf_tri.m
@@ -1,7 +1,5 @@
 function NVCP_wb_vol2surf_tri(inmetric,inmesh,output,surfinroi)
 %% transform volume to surface with trilinear methods 
-% something wrong with dHCP dataset that vertex were not aligning between white and pial surface 
-% so using trilinear methods instead of ribbon constrained
 %% individual space
 command = ['wb_command -volume-to-surface-mapping ',inmetric,' ',inmesh,...
     ' ',output,' -trilinear'];    
